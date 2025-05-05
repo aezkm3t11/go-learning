15edf22fc0155fa8413d87485d8fcad5cfaa78fd
Vyacheslav Asalkhanov <60407027+aezkm3t11@users.noreply.github.com>
2025-05-05 16:31:09 -0700

    Create local_global.go

---
+++ local_global.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var globalVar := "Я глобальная переменнная!"
+func (main) {
+  localVar := "а я локальная переменнная, объявлена внутри функции main()"
+
+  fmt.Println (globalVar)
+  fmt.Println (localVar)
+  
+}
+
+//локальная и глобальная переменные
